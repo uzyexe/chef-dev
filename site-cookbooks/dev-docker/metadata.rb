f31f27b8af1bc92bb444da9cdbf05704bedb590b
uzyexe <uzy.exe@gmail.com>
2015-08-30 18:59:35 +0900

    Docker

---
+++ site-cookbooks/dev-docker/metadata.rb
@@ -0,0 +1,9 @@
+name             'dev-docker'
+maintainer       'uzy'
+maintainer_email 'uzy.exe@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures dev-docker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'docker'
---
+++ site-cookbooks/dev-docker/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: dev-docker
+# Recipe:: default
+#
+# Copyright 2015, uzy
+#
+# All rights reserved - Do Not Redistribute
+#
+
+docker_image 'registry' do
+  tag 'latest'
+  action :pull
+  notifies :redeploy, 'docker_container[registry]'
+end
+
+docker_container 'registry' do
+  repo 'registry'
+  tag 'latest'
+  env [
+    'STANDALONE=false',
+    'MIRROR_SOURCE=https://registry-1.docker.io',
+    'MIRROR_SOURCE_INDEX=https://index.docker.io',
+    'SQLALCHEMY_INDEX_DATABASE=sqlite:////tmp/registry/docker-registry.db',
+    'SQLALCHEMY_INDEX_DATABASE=sqlite:////tmp/registry/docker-registry.db'
+  ]
+  dns ['8.8.8.8', '8.8.4.4']
+  port '5000:5000'
+  volumes ['/tmp/registry:/tmp/registry']
+  action :run
+end
+
+docker_service 'default' do
+  host 'unix:///var/run/docker.sock'
+  registry_mirror "http://#{node['ipaddress']}:5000"
+  action [:create, :start]
+end
